7cf35ecbbb79af29d5cda6807f410c805245dd07
HuLiGan203 <125124223+HuLiGan203@users.noreply.github.com>
2023-03-19 18:11:23 +0300

    Add files via upload

---
+++ module/src/main/cpp/functions.h
@@ -0,0 +1,45 @@
+namespace Vars {
+     namespace Esp {
+	 static bool start = false;
+	 static bool line = false;
+	 static bool box = false;	
+	 static bool distance = false;
+	 static bool hp = false;
+	 static bool crosshair = false;
+	 static bool nickname = false;
+	 static float ThicknessLine = 2.0f;
+     static float EnemyLineX = 0.0f;
+     static float EnemyLineY = 0.0f;
+} 
+	 namespace Player {
+	 bool func1 = false;
+	 float func2 = 0.0f;
+	 static bool Chams1 = false;
+     static bool Chams2 = false;
+     static bool Chams3 = false;
+     static bool Chams4 = false;
+     static bool Chams5 = false;
+     static bool Chams6 = false;
+     static bool Chams7 = false;
+     static bool Chams8 = false;
+
+     static int Color1 = 0;
+     static int Color2 = 0;
+     static int Color3 = 0;
+     static int Color4 = 0;
+	
+}
+     namespace Weapon {
+	 bool func3 = false;
+	 bool func4 = false;
+	 bool func5 = false;
+	 bool func6 = false;
+}
+     namespace Other {
+	 bool func7 = false;
+	 bool func8 = false;
+	 bool func9 = false;
+	 bool func10 = false;
+}
+
+}
