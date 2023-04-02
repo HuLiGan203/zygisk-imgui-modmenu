f0b90223a9cc90acdef845aa85fa566f20ec0a01
HuLiGan203 <125124223+HuLiGan203@users.noreply.github.com>
2023-04-02 13:55:39 +0300

    Update Logger.h

--- module/src/main/cpp/Includes/Logger.h
+++ module/src/main/cpp/Includes/Logger.h
@@ -17,11 +17,10 @@ enum LogType {
     oWARN  = 5
 };
 
-#define TAG OBFUSCATE("Askan")
-
-#define LOGD(...) ((void)__android_log_print(oDEBUG, TAG, __VA_ARGS__))
-#define LOGE(...) ((void)__android_log_print(oERROR, TAG, __VA_ARGS__))
-#define LOGI(...) ((void)__android_log_print(oINFO,  TAG, __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(oWARN,  TAG, __VA_ARGS__))
+#define LOG_TAG "Askan"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 #endif /* Logger_h */
