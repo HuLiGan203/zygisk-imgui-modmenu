bd83a9a961f294b97a5bd57b169836636fbc49c1
HuLiGan203 <125124223+HuLiGan203@users.noreply.github.com>
2023-06-09 23:43:28 +0300

    Update Includes.h

--- module/src/main/cpp/ByNameModding/Includes.h
+++ module/src/main/cpp/ByNameModding/Includes.h
@@ -23,18 +23,3 @@
 
 #include <jni.h>
 #include <android/log.h>
-
-#define IS_DEBUG
-
-#if defined(IS_DEBUG)
-#define LOG_TAG "DarkTeam_LOG"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-#else
-#define LOGI(...)
-#define LOGD(...)
-#define LOGW(...)
-#define LOGE(...)
-#endif
